d5689df8ee509e08c87bcd49b4b145bda00e3ad0
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-02-10 21:56:41 -0500

    started cors install

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cart = require('./routes/cart');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/cart', cart);
